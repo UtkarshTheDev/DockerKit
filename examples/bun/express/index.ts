039b188b2affaf9e64093e2d1fcff97b7e627744
Utkarsh Tiwari <utkarshweb2023@gmail.com>
2025-12-05 22:34:24 +0530

    🚀 feat: Add Dockerized Express server example for Bun - Provides a working Express server example that runs in a Docker container with Bun

--- examples/bun/express/index.ts
+++ examples/bun/express/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Welcome from a Dockerized express Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server runnning on ${PORT} in Dockerized Container`);
+});
